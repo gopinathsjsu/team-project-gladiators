9d0a2988559018cde727b78e3ea7d92e46b75bfe
Napoleon Salazar <nsalazar0429@gmail.com>
2024-03-04 01:51:57 -0800

    GC-12 Add github action to check for formatting, test & compilation

---
+++ front_end/app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        val operation = 2 + 2
+        assertThat(operation).isEqualTo(4)
+    }
+}
