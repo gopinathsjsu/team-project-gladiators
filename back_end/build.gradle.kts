3c3541c346aaf20c55b40e8e5de10d3fd564df48
vipvivek15 <69840844+vipvivek15@users.noreply.github.com>
2024-03-10 19:14:19 -0700

    Adding CI

--- back_end/build.gradle.kts
+++ back_end/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.springframework.boot") version "3.2.3"
     id("io.spring.dependency-management") version "1.1.4"
     jacoco
+
 }
 
 springBoot {
@@ -11,6 +12,7 @@ springBoot {
 
 group = "org.example"
 version = "0.0.1-SNAPSHOT"
+
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
@@ -29,14 +31,11 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    // Other dependencies...
     // JUnit Jupiter for testing
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
-
-    // Spring Boot test starters (if you're using Spring Boot)
+    // Spring Boot test starters
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-
-    // Mocking framework (if needed)
+    // Mocking framework
     testImplementation("org.mockito:mockito-core:4.0.0")
 }
 
@@ -44,3 +43,20 @@ tasks.withType<Test> {
     useJUnitPlatform()
 }
 
+tasks.test {
+    finalizedBy("jacocoTestReport") // generates the coverage report after tests run
+}
+
+tasks.jacocoTestCoverageVerification {
+    dependsOn("test")
+    violationRules {
+        rule {
+            limit {
+                minimum = "0.80".toBigDecimal()  // 80% coverage
+            }
+        }
+    }
+}
+tasks.build {
+    dependsOn(tasks.jacocoTestCoverageVerification)
+}
