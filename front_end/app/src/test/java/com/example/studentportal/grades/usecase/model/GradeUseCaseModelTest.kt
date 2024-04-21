0aa3b1f67aded17f446ef4b4b463bb31176a3049
gemj98 <gemj98@gmail.com>
2024-04-21 09:47:40 -0700

    Add tests for GradeList frontend

---
+++ front_end/app/src/test/java/com/example/studentportal/grades/usecase/model/GradeUseCaseModelTest.kt
@@ -0,0 +1,43 @@
+package com.example.studentportal.grades.usecase.model
+
+import com.example.studentportal.grades.ui.model.GradeListUiModel
+import com.example.studentportal.grades.ui.model.GradeUiModel
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class GradeUseCaseModelTest {
+
+    @Test
+    fun `test GradeList model`() {
+        // Arrange
+        val gradeListUseCaseModel = GradeListUseCaseModel(
+            listOf(
+                GradeUseCaseModel(
+                    gradeId = "1",
+                    score = 10,
+                    studentFirstName = "First-N1",
+                    studentLastName = "Last-N1",
+                    studentId = "1"
+                )
+            )
+        )
+
+        // Act
+        val result = gradeListUseCaseModel.toUiModel()
+
+        // Assert
+        assertThat(result).isEqualTo(
+            GradeListUiModel(
+                listOf(
+                    GradeUiModel(
+                        gradeId = "1",
+                        score = 10,
+                        studentFirstName = "First-N1",
+                        studentLastName = "Last-N1",
+                        studentId = "1"
+                    )
+                )
+            )
+        )
+    }
+}
