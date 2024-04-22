0fd2d299cc2515385a1695b07c584fb05fd5552e
vipvivek15 <69840844+vipvivek15@users.noreply.github.com>
2024-04-22 15:23:56 -0700

    Fixing tests

--- front_end/app/src/test/java/com/example/studentportal/courses/CourseActivityTests.kt
+++ front_end/app/src/test/java/com/example/studentportal/courses/CourseActivityTests.kt
@@ -6,12 +6,17 @@ import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth.assertThat
+import org.junit.After
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
 
 @RunWith(AndroidJUnit4::class)
 class CourseActivityTests {
-
+    @After
+    fun tearDown() {
+        stopKoin()
+    }
     @Test
     fun activityLaunchesSuccessfully() {
         val intent = Intent(ApplicationProvider.getApplicationContext(), CourseActivity::class.java)
