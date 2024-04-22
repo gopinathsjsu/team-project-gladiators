d00496ea2b4beb5159cb2b4f1b2e0ec497536bf2
gemj98 <gemj98@gmail.com>
2024-04-22 14:17:34 -0700

    Adding Vivek's code

---
+++ front_end/app/src/test/java/com/example/studentportal/courses/CourseActivityTests.kt
@@ -0,0 +1,25 @@
+package com.example.studentportal.courses
+
+import android.content.Intent
+import androidx.lifecycle.Lifecycle
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class CourseActivityTests {
+
+    @Test
+    fun activityLaunchesSuccessfully() {
+        val intent = Intent(ApplicationProvider.getApplicationContext(), CourseActivity::class.java)
+        intent.action = Intent.ACTION_MAIN
+        intent.addCategory(Intent.CATEGORY_LAUNCHER)
+
+        ActivityScenario.launch<CourseActivity>(intent).use { scenario ->
+            assertThat(scenario.state).isEqualTo(Lifecycle.State.RESUMED)
+        }
+    }
+}
