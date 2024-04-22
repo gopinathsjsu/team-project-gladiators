d378be3087180bbac6c41a811a2115bb1ba8f5ae
vipvivek15 <69840844+vipvivek15@users.noreply.github.com>
2024-04-22 11:44:33 -0700

    Updating changes for course ID

--- back_end/src/main/java/org/example/cmpe202_final/repository/user/UserRepository.java
+++ back_end/src/main/java/org/example/cmpe202_final/repository/user/UserRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public interface UserRepository extends MongoRepository<User, String> {
     List<User> findByType(UserType type);
+    List<User> findByFirstNameIn(Set<String> names);
 }
