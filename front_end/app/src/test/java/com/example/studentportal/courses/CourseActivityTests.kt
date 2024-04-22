334460be94b726c93630bd69c01c6633eb084458
vipvivek15 <69840844+vipvivek15@users.noreply.github.com>
2024-04-22 15:51:23 -0700

    Updating ability to click on a user profile

---
+++ front_end/app/src/test/java/com/example/studentportal/courses/CourseActivityTests.kt
@@ -0,0 +1,31 @@
+package com.example.studentportal.courses
+
+import android.content.Intent
+import androidx.lifecycle.Lifecycle
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth.assertThat
+import org.junit.After
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
+
+@RunWith(AndroidJUnit4::class)
+class CourseActivityTests {
+    @After
+    fun tearDown() {
+        stopKoin()
+    }
+
+    @Test
+    fun activityLaunchesSuccessfully() {
+        val intent = Intent(ApplicationProvider.getApplicationContext(), CourseActivity::class.java)
+        intent.action = Intent.ACTION_MAIN
+        intent.addCategory(Intent.CATEGORY_LAUNCHER)
+
+        ActivityScenario.launch<CourseActivity>(intent).use { scenario ->
+            assertThat(scenario.state).isEqualTo(Lifecycle.State.RESUMED)
+        }
+    }
+}
