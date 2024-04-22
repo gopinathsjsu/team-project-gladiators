841b3aeb9ebadf95125d720db8867122f0e5cc81
mateajinkya <93010008+mateajinkya@users.noreply.github.com>
2024-04-21 22:30:29 -0700

    Fixed another ktlint issue...because ktlint is unnecessarily strict.

--- front_end/app/src/test/java/com/example/studentportal/profile/ui/fragment/UserProfileViewModelTest.kt
+++ front_end/app/src/test/java/com/example/studentportal/profile/ui/fragment/UserProfileViewModelTest.kt
@@ -19,7 +19,6 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-
 class UserProfileViewModelTest {
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
