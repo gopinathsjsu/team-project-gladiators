ad8eef035c57cda7f46ac0cd660437e03da64b55
vipvivek15 <69840844+vipvivek15@users.noreply.github.com>
2024-05-01 21:32:24 -0700

    Fetching students from course ID

--- back_end/src/main/java/org/example/cmpe202_final/repository/user/UserRepository.java
+++ back_end/src/main/java/org/example/cmpe202_final/repository/user/UserRepository.java
@@ -3,11 +3,16 @@ package org.example.cmpe202_final.repository.user;
 import org.example.cmpe202_final.model.user.User;
 import org.example.cmpe202_final.model.user.UserType;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public interface UserRepository extends MongoRepository<User, String> {
     List<User> findByType(UserType type);
     User findByEmail(String email);
+    @Query("{'_id': {$in: ?0}, 'type': 'STUDENT'}")
+    List<User> findStudentsByEnrolledIds(Set<String> studentIds);
+
 }
--- back_end/src/main/java/org/example/cmpe202_final/service/course/CourseService.java
+++ back_end/src/main/java/org/example/cmpe202_final/service/course/CourseService.java
@@ -2,18 +2,23 @@ package org.example.cmpe202_final.service.course;
 
 import lombok.AllArgsConstructor;
 import org.example.cmpe202_final.model.course.Course;
+import org.example.cmpe202_final.model.user.User;
 import org.example.cmpe202_final.repository.courses.CourseRepository;
+import org.example.cmpe202_final.repository.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @AllArgsConstructor
 @Service
 public class CourseService {
 
     private final CourseRepository courseRepository;
+
+    private final UserRepository userRepository;
 
     public List<Course> findAllCourses(){
         return courseRepository.findAllBy();
@@ -25,5 +30,15 @@ public class CourseService {
 
     public List<Course> findByInstructor(String instructor){
         return courseRepository.findByInstructor(instructor);
+    }
+    public List<User> findStudentsByCourseId(String courseId) {
+        // Fetch the course by ID
+        Course course = courseRepository.findById(courseId).orElseThrow(() -> new IllegalArgumentException("Course not found"));
+
+        // Get the enrolled student IDs from the course
+        Set<String> enrolledStudentIds = course.getEnrolledStudents();
+
+        // Fetch and return the student Users
+        return userRepository.findStudentsByEnrolledIds(enrolledStudentIds);
     }
 }
