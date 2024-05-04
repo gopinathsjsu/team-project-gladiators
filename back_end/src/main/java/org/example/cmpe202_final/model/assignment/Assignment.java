6e27cb5155befb8e6cc922e06e9355b2d854326d
gemj98 <gemj98@gmail.com>
2024-05-03 22:14:54 -0700

    Create a `Grade` object for each `Student` when an `Assignment` is created

--- back_end/src/main/java/org/example/cmpe202_final/model/assignment/Assignment.java
+++ back_end/src/main/java/org/example/cmpe202_final/model/assignment/Assignment.java
@@ -1,9 +1,5 @@
 package org.example.cmpe202_final.model.assignment;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
